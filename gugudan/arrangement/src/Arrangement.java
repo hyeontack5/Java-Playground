df0ef9aff7b5f44baa56407cb427f69bc252dc39
hyeontack5 <sksgusxor@gmail.com>
2021-11-20 23:28:23 +0900

    Add: 배열로 구구단 다시 구현

---
+++ gugudan/arrangement/src/Arrangement.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Arrangement {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("출력할 구구단을 입력하세요: ");
+        int dan = scanner.nextInt();
+
+        int[] result = new int[9];
+
+        for (int i = 0; i < result.length; i++) {
+            result[i] = dan * (i+1);
+            System.out.println(dan + " * " + (i+1) + " = " + result[i]);
+        }
+    }
+}
