bb6104e68d62d0ba56f3c76c4637b4336886ec2e
hyeontack5 <sksgusxor@gmail.com>
2021-11-25 22:28:36 +0900

    Refactor: 가상의 달력 찍기2

--- calendar/src/calendar/Prompt.java
+++ calendar/src/calendar/Prompt.java
@@ -3,18 +3,18 @@ package calendar;
 import java.util.Scanner;
 
 public class Prompt {
-
-    private final static String PROMPT = "> ";
-
     public void runPrompt() {
+
         // 숫자를 입력받아 해당하는 달의 최대 일수를 출력하는 프로그램
         Scanner scanner = new Scanner(System.in);
         Calendar cal = new Calendar();
 
         while (true) {
-            System.out.println("년과 월을 입력하세요.");
-            System.out.print(PROMPT);
+            System.out.println("년을 입력하세요.");
+            System.out.print("YEAR> ");
             int year = scanner.nextInt();
+            System.out.println("월을 입력하세요.");
+            System.out.print("MONTH> ");
             int month = scanner.nextInt();
 
             if (month == -1) {
