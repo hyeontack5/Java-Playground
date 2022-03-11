c796e0c556f5ea665208239d08e1e28fa9b976ca
hyeontack5 <sksgusxor@gmail.com>
2022-03-11 22:55:34 +0900

    Feat: 일정등록, 검색 기능 구현하기(1)

--- calendar/src/calendar/Prompt.java
+++ calendar/src/calendar/Prompt.java
@@ -4,8 +4,16 @@ import java.util.Scanner;
 
 public class Prompt {
 
+    public void printMenu() {
+        System.out.println("+-------------------+");
+        System.out.println("| 1. 일정등록   ");
+        System.out.println("| 2. 일정검색   ");
+        System.out.println("| 3. 달력보기   ");
+        System.out.println("| h. 도움말 q. 종료");
+        System.out.println("+------------------+");
+    }
+
     /**
-     *
      * @param week 요일명
      * @return 0 ~ 6 (0 = Sunday, 6 = Saturday)
      */
@@ -30,37 +38,50 @@ public class Prompt {
     }
 
     public void runPrompt() {
-
+        printMenu();
         // 숫자를 입력받아 해당하는 달의 최대 일수를 출력하는 프로그램
         Scanner scanner = new Scanner(System.in);
         Calendar cal = new Calendar();
 
-        int month = 1;
-        int year = 2017;
 
         while (true) {
-            System.out.println("년을 입력하세요. (exit: -1)");
-            System.out.print("YEAR> ");
-            year = scanner.nextInt();
-            if (year == -1) {
-                System.out.println("Have a nice day!");
-                break;
-            }
-
-            System.out.println("월을 입력하세요.");
-            System.out.print("MONTH> ");
-            month = scanner.nextInt();
-
-            if (month < 1 || month > 12) {
-                System.out.println("잘못된 입력입니다.");
-                continue;
-            }
-
-            cal.printCalendar(year, month);
+            System.out.println("명령 (1, 2, 3, h, q)");
+            String cmd = scanner.next();
+            if (cmd.equals("1")) cmdRegister();
+            else if (cmd.equals("2")) cmdSearch();
+            else if (cmd.equals("3")) cmdCal(scanner, cal);
+            else if (cmd.equals("h")) printMenu();
+            else if (cmd.equals("q")) break;
         }
 
-        System.out.println("Bye~");
+        System.out.println("Thank you. Bye~");
         scanner.close();
+    }
+
+    private void cmdCal(Scanner s, Calendar c) {
+        int month = 1;
+        int year = 2017;
+        System.out.println("년을 입력하세요.");
+        System.out.print("YEAR> ");
+        year = s.nextInt();
+
+        System.out.println("월을 입력하세요.");
+        System.out.print("MONTH> ");
+        month = s.nextInt();
+
+        if (month < 1 || month > 12) {
+            System.out.println("잘못된 입력입니다.");
+            return;
+        }
+
+        c.printCalendar(year, month);
+
+    }
+
+    private void cmdSearch() {
+    }
+
+    private void cmdRegister() {
     }
 
     public static void main(String[] args) {
